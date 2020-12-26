0c82c3affdde936da99c911d3828f49940cb043d
Jerry Devin <jerrydevin96@gmail.com>
2020-12-26 12:29:19 +0530

    rough framework created

---
+++ http-handler.go
@@ -0,0 +1,38 @@
+package rest
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	client HTTPClient = &http.Client{}
+)
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func executeAPI(method string, apiURL string, headers []*Headers, payload io.Reader) (int, string, error) {
+	request, err := http.NewRequest(method, apiURL, payload)
+	if err != nil {
+		log.Panic(err)
+		return 0, "", err
+	}
+
+	for i := 0; i < len(headers); i++ {
+		header := headers[i]
+		request.Header.Add(header.Key, header.Value)
+	}
+
+	response, err := client.Do(request)
+
+	statusCode := response.StatusCode
+	defer response.Body.Close()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+	responseString := string(responseBody)
+
+	return statusCode, responseString, err
+}
--- rest-simplified.go
+++ rest-simplified.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"bytes"
+	"io"
 	"net/url"
 )
 
@@ -10,13 +12,11 @@ type Headers struct {
 }
 
 type APIClient struct {
-	APIURL           string
-	CustomAPIHeaders []*Headers
-	ContentType      string
-	Authorization    string
-	APIType          string
-	AuthType         string
-	AuthToken        string
+	APIURL               string
+	AdditionalAPIHeaders []*Headers
+	ContentType          string
+	Authorization        string
+	APIMethod            string
 }
 
 func CreateBearerAuth(token string) string {
@@ -27,4 +27,57 @@ func CreateBearerAuth(token string) string {
 func CreateBasicAuth(userName string, password string) string {
 	authToken := "Basic " + userName + ":" + url.QueryEscape(password)
 	return authToken
+}
+
+func CreateAPIClient(apiUrl string, apiMethod string, authorization string, contentType string) *APIClient {
+	apiClient := &APIClient{}
+	apiClient.APIURL = apiUrl
+	apiClient.APIMethod = apiMethod
+	apiClient.Authorization = authorization
+	apiClient.ContentType = contentType
+	return apiClient
+}
+
+func (client *APIClient) AddAdditionalRequestHeader(headerName string, headerValue string) *APIClient {
+	header := &Headers{}
+	header.Key = headerName
+	header.Value = headerValue
+	clientAPIHeaders := client.AdditionalAPIHeaders
+	clientAPIHeaders = append(clientAPIHeaders, header)
+	client.AdditionalAPIHeaders = clientAPIHeaders
+	return client
+}
+
+func (client *APIClient) ExecuteAPI(JSONPayload string) (int, string, error) {
+	var apiMethod string
+	var apiURL string
+	var contentType string
+	var authorization string
+	var payload io.Reader = nil
+
+	apiMethod = client.APIMethod
+	apiURL = client.APIURL
+	contentType = client.ContentType
+	authorization = client.Authorization
+	headers := client.AdditionalAPIHeaders
+
+	contentTypeHeader := &Headers{}
+	contentTypeHeader.Key = "content-type"
+	contentTypeHeader.Value = contentType
+	headers = append(headers, contentTypeHeader)
+
+	if authorization != "none" || authorization != "" {
+		authorizationHeader := &Headers{}
+		authorizationHeader.Key = "Authorization"
+		authorizationHeader.Value = authorization
+		headers = append(headers, authorizationHeader)
+	}
+
+	if JSONPayload != "" {
+		payload = bytes.NewBuffer([]byte(JSONPayload))
+	}
+
+	responseCode, responseBody, err := executeAPI(apiMethod, apiURL, headers, payload)
+
+	return responseCode, responseBody, err
 }
